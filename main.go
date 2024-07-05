6578d1d6cfa603976a802e88e44da166b98b3cca
Joe <jmaurer1994@gmail.com>
2024-07-05 10:48:14 -0400

    Add more stats commands

--- main.go
+++ main.go
@@ -225,9 +225,26 @@ func onChannelMessage(shardID int, msg irc.ChatMessage) {
 					dailyStats, err := db.RetrieveDailyStats()
 					if err != nil {
 						log.Printf("Error retrieving daily stats: %v", err)
+						tic.SendChannelMessage("Error retrieving daily stats")
 						return
 					}
 					tic.SendChannelMessage(fmt.Sprintf("Daily Stats: Food was dispensed %d times today with a max/min/avg force of %f/%f/%f", dailyStats.Day_event_count, dailyStats.Day_max_force, dailyStats.Day_min_force, dailyStats.Day_avg_force))
+				case "weekly":
+					weeklyStats, err := db.RetrieveWeeklyStats()
+					if err != nil {
+						log.Printf("Error retrieving weekly stats: %v", err)
+						tic.SendChannelMessage("Error retrieving weekly stats")
+						return
+					}
+					tic.SendChannelMessage(fmt.Sprintf("Weekly Stats: Food was dispensed %d times this week with an average of %f per day, having a max/min/avg force of %f/%f/%f", weeklyStats.Week_total_events, weeklyStats.Daily_avg_events, weeklyStats.Week_max_force, weeklyStats.Week_min_force, weeklyStats.Week_avg_force))
+				case "monthly":
+					monthlyStats, err := db.RetrieveMonthlyStats()
+					if err != nil {
+						log.Printf("Error retrieving monthly stats: %v", err)
+						tic.SendChannelMessage("Error retrieving monthly stats")
+						return
+					}
+					tic.SendChannelMessage(fmt.Sprintf("Monthly Stats: Food was dispensed %d times this Month with an average of %f per day, having a max/min/avg force of %f/%f/%f", monthlyStats.Month_total_events, monthlyStats.Daily_avg_events, monthlyStats.Month_max_force, monthlyStats.Month_min_force, monthlyStats.Month_avg_force))
 				}
 			}
 		}
