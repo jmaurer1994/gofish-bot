91dbbc96c6ab16d764f8f585a9696af40e2a66b0
Joe <jmaurer1994@gmail.com>
2024-07-05 11:11:23 -0400

    update decimal formatting for stats

--- main.go
+++ main.go
@@ -228,7 +228,7 @@ func onChannelMessage(shardID int, msg irc.ChatMessage) {
 						tic.SendChannelMessage("Error retrieving daily stats")
 						return
 					}
-					tic.SendChannelMessage(fmt.Sprintf("Daily Stats: Food was dispensed %d times today with a max/min/avg force of %f.0/%f.0/%f.0", dailyStats.Day_event_count, dailyStats.Day_max_force, dailyStats.Day_min_force, dailyStats.Day_avg_force))
+					tic.SendChannelMessage(fmt.Sprintf("Daily Stats: Food was dispensed %d times today with a max/min/avg force of %.0f/%.0f/%.0f", dailyStats.Day_event_count, dailyStats.Day_max_force, dailyStats.Day_min_force, dailyStats.Day_avg_force))
 				case "weekly":
 					weeklyStats, err := db.RetrieveWeeklyStats()
 					if err != nil {
@@ -236,7 +236,7 @@ func onChannelMessage(shardID int, msg irc.ChatMessage) {
 						tic.SendChannelMessage("Error retrieving weekly stats")
 						return
 					}
-					tic.SendChannelMessage(fmt.Sprintf("Weekly Stats: Food was dispensed %d times this week with an average of %f.0 per day, having a max/min/avg force of %f.0/%f.0/%f.0", weeklyStats.Week_total_events, weeklyStats.Daily_avg_events, weeklyStats.Week_max_force, weeklyStats.Week_min_force, weeklyStats.Week_avg_force))
+					tic.SendChannelMessage(fmt.Sprintf("Weekly Stats: Food was dispensed %d times this week with an average of %.0f per day, having a max/min/avg force of %.0f/%.0f/%.0f", weeklyStats.Week_total_events, weeklyStats.Daily_avg_events, weeklyStats.Week_max_force, weeklyStats.Week_min_force, weeklyStats.Week_avg_force))
 				case "monthly":
 					monthlyStats, err := db.RetrieveMonthlyStats()
 					if err != nil {
@@ -244,7 +244,7 @@ func onChannelMessage(shardID int, msg irc.ChatMessage) {
 						tic.SendChannelMessage("Error retrieving monthly stats")
 						return
 					}
-					tic.SendChannelMessage(fmt.Sprintf("Monthly Stats: Food was dispensed %d times this Month with an average of %f.0 per day, having a max/min/avg force of %f.0/%f.0/%f.0", monthlyStats.Month_total_events, monthlyStats.Daily_avg_events, monthlyStats.Month_max_force, monthlyStats.Month_min_force, monthlyStats.Month_avg_force))
+					tic.SendChannelMessage(fmt.Sprintf("Monthly Stats: Food was dispensed %d times this Month with an average of %.0f per day, having a max/min/avg force of %.0f/%.0f/%.0f", monthlyStats.Month_total_events, monthlyStats.Daily_avg_events, monthlyStats.Month_max_force, monthlyStats.Month_min_force, monthlyStats.Month_avg_force))
 				}
 			}
 		}
